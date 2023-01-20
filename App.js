c2df4cf1cdc9047a5a88425db100d39ec7c6fa37
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-20 11:59:24 -0300

    Desenvolvendo o layout

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Input } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={estilos.safeContainer}>
+        <Text style={estilos.tituloExercicio}>
+          App 1 - Fotos de lugares visitados
+        </Text>
+
+        <View style={estilos.containerNomeLocal}>
+          <TextInput
+            style={estilos.nomeLocal}
+            placeholder="Titulo da foto/local"
+          />
+        </View>
+
+        <View style={estilos.foto}></View>
+      </SafeAreaView>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const estilos = StyleSheet.create({
+  safeContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f4f4f4",
+  },
+  tituloExercicio: {
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 50,
+  },
+  nomeLocal: {
+    borderWidth: 1,
+    fontSize: 16,
+    marginTop: 20,
+    height: 50,
+    width: "90%",
+    textAlign: "center", // Deixa o placheholder centralizado
+  },
+  containerNomeLocal: {
     alignItems: "center",
-    justifyContent: "center",
+  },
+  mapa: {
+    backgroundColor: "#f0f",
+    height: 20,
+    width: 20,
   },
 });
