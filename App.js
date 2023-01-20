af2259bb8c2aeff946f0b9d2ec67e86ada5ee179
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-20 11:12:11 -0300

    teste

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, Input } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Input>oi</Input>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
