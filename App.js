293737da4bdcf5b0626b1a9240033bb1e86c68fd
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2023-01-20 23:07:54 -0300

    Parte visual Finalizada

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from "expo-status-bar";
+import { cloneElement } from "react";
 import {
   StyleSheet,
   Text,
@@ -8,10 +9,12 @@ import {
   SafeAreaView,
 } from "react-native";
 
+
 export default function App() {
   return (
     <>
       <SafeAreaView style={estilos.safeContainer}>
+
 
         <Text style={estilos.tituloExercicio}>
           App 1 - Fotos de lugares visitados
@@ -26,8 +29,17 @@ export default function App() {
 
         <View style={estilos.foto}></View>
 
-        <Pressable><Text>oi</Text></Pressable>
-        
+        <Pressable style={estilos.botaoFoto}>
+          <Text style={estilos.textoBotaoFoto}>Tirar Foto</Text>
+        </Pressable>
+
+        <View style={estilos.mapa}></View>
+
+        <Pressable style={estilos.botaoLocalizar}>
+          <Text style={estilos.textoLocalizar}>Localizar no mapa</Text>
+        </Pressable>
+
+
       </SafeAreaView>
     </>
   );
@@ -45,9 +57,9 @@ const estilos = StyleSheet.create({
   },
   campoLocal: {
     borderWidth: 1,
-    fontSize: 16,
-    marginTop: 20,
-    height: 50,
+    fontSize: 18,
+    marginTop: 25,
+    height: 54,
     width: "90%",
     textAlign: "center", // Deixa o placheholder centralizado
   },
@@ -55,13 +67,46 @@ const estilos = StyleSheet.create({
     alignItems: "center",
   },
   foto: {
-    marginTop: 30,
+    marginTop: 20,
     width: "90%",
     height: 200,
     backgroundColor: "#f0f",
     justifyContent: "center",
     marginLeft: 20 
   },
+  botaoFoto: {
+    marginTop: 20,
+    borderWidth: 1,
+    height: 54,
+    width: "90%",
+    marginLeft: 20
+  },
+  textoBotaoFoto: {
+    fontSize: 20,
+    alignSelf: "center",
+    marginTop: 10
+  },
+  mapa: {
+    marginTop: 50,
+    width: "90%",
+    height: 200,
+    backgroundColor: "#f0f",
+    justifyContent: "center",
+    marginLeft: 20 
+  },
+  botaoLocalizar: {
+    marginTop: 20,
+    borderWidth: 1,
+    height: 54,
+    width: "90%",
+    marginLeft: 20
+  },
+  textoLocalizar: {
+    fontSize: 20,
+    alignSelf: "center",
+    marginTop: 10
+  }
+   
 
 
   
